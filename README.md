2d571b0929b114af5d61a3a28e96d7153352707a
Will <will@Will.(none)>
2012-10-09 08:11:12 +0200

    basic function syntax

--- README.md
+++ README.md
@@ -35,12 +35,12 @@ Duckable checks can contain duckable attributes themselves too:
     def example4(person: {"name":str, "phone": {"type":str, "number":str}}):
         ...
         
-You can specify alternative constraint types using tuples:
+You can specify alternative constraint types using sets:
 
-    def example5(x: (int,float)):
+    def example5(x: {int,float}):
         ...
         
-In fact, *number* type is just a tuple of int and float.  And *noneable* is just an alternative way of saying (...,NoneType)
+In fact, *number* type is just a set of int and float.  And *noneable* is just a way of saying {...,None}
 
 Lists mean that the attribute must be an array where each element matches the constraint e.g.:
 
@@ -48,6 +48,30 @@ Lists mean that the attribute must be an array where each element matches the co
         ...
         
 You can provide your own complex custom constraint checkers by subclassing the ObiwanCheck class; look at obiwan.StringCheck for inspiration.
+
+You can say that a parameter is callable using function:
+
+    def example7(callback: function):
+        ...
+        
+If you want, you can describe the parameters that the function should take:
+
+    def example8(callback: function(int,str)):
+        ...
+        
+However, all the functions passed to example8 must now be properly annotated with a matching annotation.
+
+The special type any can be used if you do not want to check the type:
+
+    def example9(callback: function(int,any,number)):
+        ...
+        
+You can also specify that a function should support further arguments using ellipsis:
+
+    def example10(callback: function(int,any,...)):
+        ...
+        
+This will ensure that all callbacks have at least two parameters, the first being an int.
 
 Utility functions to load and dump JSON are provided.  These support a new *template* parameter and validate the input/output matches the constraint e.g.:
 
--- obiwan.py
+++ obiwan.py
@@ -1,13 +1,49 @@
+import inspect
 
 class ObiwanError(Exception):
     "Thrown by Obiwan checker if a function call or object definition does not match at runtime"
 
-class ObiwanCheck(object):
+class ObiwanCheck:
     "subclass this for custom checks"
     def check(self,obj,ctx):
         "perform your custom check here; if fails, please throw a ObiwanError"
         raise NotImplementedError("subclasses of ObiwanCheck must override check()")
         
+class function(ObiwanCheck):
+    """something that is callable; there are two ways to use this class:
+        1) arg: function, ...
+        in this case arg must be callable; the signature is not checked
+        2) arg: function(int,float,...)
+        the signature is checked; all functions passed must have this type signature
+    """
+    def __init__(self,*args):
+        self.ellipsis = args.count(Ellipsis)
+        if self.ellipsis:
+            assert self.ellipsis == 1, "Ellipsis can only occur at end of function types"
+            assert args[-1] == Ellipsis, "Ellipsis can only occur at end of function types"
+            self.args = args[:-1]
+        else:
+            self.args = args
+    def check(self,obj,ctx):
+        if not hasattr(obj,"__call__"):
+            raise ObiwanError("%s is not callable"%ctx)
+        args = inspect.getfullargspec(obj)
+        if not args.annotations:
+            raise ObiwanError("%s is not annotated"%ctx)
+        if len(args.args) < len(self.args):
+            raise ObiwanError("%s has too few arguments"%ctx)
+        if not self.ellipsis and (len(args.args) > len(self.args)):
+            raise ObiwanError("%s has too many arguments"%ctx)
+        for arg,name in zip(self.args,args.args):
+            if (arg is any) or isinstance(arg,str):
+                continue
+            got = args.annotations.get(name,None)
+            if got is None:
+                raise ObiwanError("%s %s(%s) is not annotated"%(ctx,obj.__name__,name))
+            if (got is any) or isinstance(got,str):
+                continue
+            sametype(arg,got,"%s(%s)"%(ctx,name))
+
 class StringCheck(ObiwanCheck):
     "an example check that ensures strings are within certain size limits"
     def __init__(self,maxlen,minlen=0):
@@ -28,61 +64,90 @@ class DecimalCheck(ObiwanCheck):
         except:
             raise ObiwanError("%s is %s but should be a string containing a decimal value"%(ctx,type(obj)))
 
-class optional(object):
-    def __init__(self,template):
-        self.template = template
+class optional:
+    def __init__(self,key):
+        self.key = key
         
-class noneable(object):
+class noneable:
     def __init__(self,template):
         self.template = template
 
-number = (int,float) # a type that is a number
+number = {int,float} # a type that is a number
+
+def sametype(expect,got,ctx):
+    "raises an ObiwanError if the type declarations a and b are incompatible"
+    if expect==got:
+        return
+    if isinstance(got,set) and expect in got:
+        return
+    raise ObiwanError("%s expects %s but got %s"%(ctx,expect,got))
 
 def duckable(obj,template,ctx=""):        
-    def check(path,obj,tmpl):
-        if isinstance(tmpl,str): # allow docstrings
-            pass
-        elif isinstance(tmpl,noneable):
-            if obj is not None:
-                check(path,obj,tmpl.template)
-        elif isinstance(tmpl,ObiwanCheck):
-            tmpl.check(obj)
-        elif isinstance(tmpl,tuple): # leaf datatype multiple-choice
-            for typ in tmpl:
-                try:
-                    check(path,obj,typ)
-                    break
-                except ObiwanError:
-                    pass
-            else:
-                raise ObiwanError("%s is %s but should be one of %s"%(path,type(obj),tmpl))
-        elif isinstance(tmpl,dict):
-            if not isinstance(obj,dict):
-                raise ObiwanError("%s is %s but should be an object"%(path,type(obj)))
-            # test explicit values
-            for key,value in tmpl.items():
-                if isinstance(key,optional):
-                    key = key.template
-                    if key not in obj:
-                        continue
-                elif key not in obj:
-                    raise ObiwanError("%s should have child called %s"%(path,key))
-                check("%s[\"%s\"]"%(path,key),obj[key],value)
-        elif isinstance(tmpl,list):
-            if not isinstance(obj,(tuple,list)):
-                raise ObiwanError("%s is %s but should be a list"%(path,type(obj)))
-            assert len(tmpl)==1, "lists must all be of the same type"
-            for i,item in enumerate(obj):
-                check("%s[%s]"%(path,i),item,tmpl[0])
-        else: # single type
-            if not isinstance(obj,tmpl):
-                raise ObiwanError("%s is %s but should be %s"%(path,type(obj),tmpl))
-    check(ctx,obj,template)
+    if isinstance(template,str): # allow docstrings
+        pass
+    elif isinstance(template,noneable):
+        if obj is not None:
+            duckable(obj,template.template,ctx)
+    elif template is function:
+        if not hasattr(obj,"__call__"):
+            raise ObiwanError("%s is not callable"%ctx)
+    elif isinstance(template,ObiwanCheck):
+        template.check(obj,ctx)
+    elif isinstance(template,set): # leaf datatype multiple-choice
+        for typ in template:
+            try:
+                duckable(obj,typ,ctx)
+                break
+            except ObiwanError:
+                pass
+        else:
+            raise ObiwanError("%s is %s but should be one of %s"%(ctx,type(obj),template))
+    elif isinstance(template,dict):
+        if not isinstance(obj,dict):
+            raise ObiwanError("%s is %s but should be an object"%(ctx,type(obj)))
+        for key,value in template.items():
+            if isinstance(key,optional):
+                key = key.key
+                if key not in obj:
+                    continue
+            elif key not in obj:
+                raise ObiwanError("%s should have child called %s"%(ctx,key))
+            duckable(obj[key],value,"%s[\"%s\"]"%(ctx,key))
+    elif isinstance(template,list):
+        if not isinstance(obj,(tuple,list)):
+            raise ObiwanError("%s is %s but should be a list"%(ctx,type(obj)))
+        assert len(template)==1, "lists must all be of the same type"
+        template = template[0]
+        for i,item in enumerate(obj):
+            duckable(item,template,"%s[%s]"%(ctx,i))
+    elif isinstance(template,tuple):
+        if not isinstance(obj,(tuple,list)):
+            raise ObiwanError("%s is %s but should be packed %s"%(ctx,type(obj),template))
+        for i,expect in enumerate(template):
+            if expect is any:
+                continue
+            if expect is Ellipsis:
+                return
+            if i >= len(obj):
+                raise ObiwanError("%s[%d] %s but should be packed %s but is omitted"%(ctx,i,expect))
+            got = obj[i]
+            duckable(got,expect,"%s[%d]"%(ctx,i))
+        if len(template) != len(obj):
+            raise ObiwanError("%s is %s but should be packed %s"%(ctx,type(obj),template))
+    else: # single type
+        if not isinstance(obj,template):
+            raise ObiwanError("%s is %s but should be %s"%(ctx,type(obj),template))
     
+def is_duckable(obj,template,ctx=""):
+    try:
+        duckable(obj,template,ctx)
+        return True
+    except ObiwanError:
+        return False
 
 import json as _json
 
-class json(object):
+class json:
     "a wrapper around Python's JSON that enables validation"
     @classmethod
     def _dump(cls,func,obj,*args,**kwargs):
